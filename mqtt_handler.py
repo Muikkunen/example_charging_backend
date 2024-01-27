9105c232b8d997bf7b31823bf551e3224c9d0f62
Samuli Mononen <s.m@phnet.fi>
2024-01-27 19:59:01 +0200

    Ensure that mqtt client has connected successfully

--- mqtt_handler.py
+++ mqtt_handler.py
@@ -1,12 +1,12 @@
-import paho.mqtt.client as mqtt
-
 import json
-from pymongo import MongoClient
 import time
+
+import paho.mqtt.client as mqtt
+from pymongo import MongoClient
 
 from config import BROKER_ADDRESS, PORT, TOPIC, USERNAME, PASSWORD, DATABASE_URL
 
-# Connect to MongoDB (assuming it's running locally on the default port)
+# Connect to MongoDB
 database_client = MongoClient(
     DATABASE_URL,
     username=USERNAME,
@@ -31,18 +31,17 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
-    
+
     # Insert the data into the collection
     data = json.loads(msg.payload.decode())
-    result = collection.insert_one(data)
-
-    # Print the inserted document's ID
-    print("Inserted document ID:", result.inserted_id)
+    data["timestamp"] = msg.timestamp # Add timestamp to the data
+    collection.insert_one(data)
+    print("Added to database:", data)
 
 
 def on_error(client, userdata, error):
     print("An error occurred:", error)
-    # Handle the error or log it as needed
+
 
 def publish_message(client, message: str) -> None:
     # Publish the message to the specified topic
@@ -56,27 +55,31 @@ mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
 
-# Connect to the broker
-mqtt_client.connect(BROKER_ADDRESS, PORT, keepalive=60)
-
-# Start the MQTT loop to handle communication in the background
-mqtt_client.loop_start()
-
-# Simulate publishing a message every 2 seconds
 try:
-    time.sleep(0.1) # Ensure that connection has been established
-    message = {
-        "session_id": 1, "energy_delivered_in_kWh":30,
-        "duration_in_seconds":45, "session_cost_in_cents": 70
-    }
-    while True:
-        publish_message(mqtt_client, message)
-        time.sleep(2)
-except KeyboardInterrupt:
-    # Disconnect on keyboard interrupt
-    mqtt_client.disconnect()
-    print("Disconnected from the broker")
+    # Connect to the broker
+    mqtt_client.connect(BROKER_ADDRESS, PORT, keepalive=60)
 
-    # Close the MongoDB connection
-    database_client.close()
-    print("Closed MongoDB connection")
+    # Start the MQTT loop to handle communication in the background
+    mqtt_client.loop_start()
+
+    # Simulate publishing a message every 2 seconds
+    try:
+        time.sleep(0.1) # Ensure that connection has been established
+        message = {
+            "session_id": 1, "energy_delivered_in_kWh":30,
+            "duration_in_seconds":45, "session_cost_in_cents": 70
+        }
+        while True:
+            publish_message(mqtt_client, message)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        # Disconnect on keyboard interrupt
+        mqtt_client.disconnect()
+        print("Disconnected from the broker")
+
+        # Close the MongoDB connection
+        database_client.close()
+        print("Closed MongoDB connection")
+
+except ConnectionRefusedError as error:
+    print(f"{error}")
